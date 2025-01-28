68c3acbcccb55db6199f4add33f95602341a1581
CamErni <camille.nazareno@betterask.erni>
2025-01-28 09:15:49 +0800

    first commit

---
+++ RepositoryPattern.API/Controllers/AuthorController.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using RepositoryPattern.API.Domain.Contracts;
+using RepositoryPattern.API.Domain.Entities;
+using RepositoryPattern.API.Repositories;
+
+namespace RepositoryPattern.API.Controllers
+{
+    [ApiController]
+    [Route("api/")]
+    public class AuthorController:ControllerBase
+    {
+        private readonly IBaseRepository<Author> _authorRepository;
+        private readonly IMapper _mapper;
+
+        public AuthorController(IBaseRepository<Author> authorRepository, IMapper mapper)
+        {
+            _authorRepository = authorRepository;
+            _mapper = mapper;
+        }
+        [HttpGet("authors")]
+        public async Task<IActionResult> GetAuthors()
+        {
+            var authors=await _authorRepository.GetAll(a=>a.Books);
+            var authorDtos = _mapper.Map<List<GetAuthorDto>>(authors);
+            return Ok(authorDtos);
+        }
+
+        [HttpGet("authors/{id}")]
+        public async Task<IActionResult> GetAuthor(Guid id)
+        {
+            var author = await _authorRepository.Get(id, a => a.Books);
+            if (author == null)
+            {
+                return NotFound();
+            }
+            var authorDto = _mapper.Map<GetAuthorDto>(author);
+            return Ok(authorDto);
+        }
+
+        [HttpPost("authors")]
+        public async Task<IActionResult> CreateAuthor([FromBody] CreateAuthor createAuthor)
+        {
+            var author = _mapper.Map<Author>(createAuthor);
+            var createdAuthor = await _authorRepository.Add(author);
+            var authorDto = _mapper.Map<GetAuthorDto>(createdAuthor);
+            return CreatedAtAction(nameof(GetAuthor), new {id=authorDto.Id},authorDto);
+        }
+    }
+}
---
+++ RepositoryPattern.API/Domain/Mapping/MappingProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using RepositoryPattern.API.Domain.Contracts;
+using RepositoryPattern.API.Domain.Entities;
+
+namespace RepositoryPattern.API.Domain.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile() 
+        {
+            CreateMap<Author, GetAuthorDto>()
+                .ForMember(dest => dest.Books, opt=>opt.MapFrom(src=>src.Books));
+            CreateMap<CreateAuthor, Author>();
+            CreateMap<UpdateAuthor, Author>();
+            CreateMap<DeleteAuthor, Author>();
+
+            // Mapping for Book
+            CreateMap<Book, GetBookDto>();
+            CreateMap<CreateBook,Book>();
+            CreateMap<UpdateBook,Book>();
+            CreateMap<DeleteBook,Book>();
+            CreateMap<GetBook,Book>();
+
+        }
+    }
+}
